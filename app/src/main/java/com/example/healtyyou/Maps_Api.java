97070fd4501627e98080fadc2af1a15d5e18e073
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2020-08-15 13:01:39 +0530

    Layout files.

---
+++ app/src/main/java/com/example/healtyyou/Maps_Api.java
@@ -0,0 +1,14 @@
+package com.example.healtyyou;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Maps_Api extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps__api);
+    }
+}
